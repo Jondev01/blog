cbbcad773ed780b16781da03000ca6e73d7a327e
Jonathan Wernersson <jonathan2008@live.se>
2019-02-23 20:06:37 +0100

    added default when no posts made

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -20,23 +20,23 @@
                 <h4 class="grey-box">Popular posts</h4>
                 <ul>
                     <li>
-                        <img src="pop1.jpeg"/> 
+                        <img src="/storage/blog_images/pop1.jpeg"/> 
                         <span class="post-title">Title one</span><br/>
                         <span>Description one</span> <br/>
                     </li>
 
                     <li>
-                        <img src="pop2.jpg"/><span class="post-title">Title two</span><br/>
+                        <img src="/storage/blog_images/pop2.jpg"/><span class="post-title">Title two</span><br/>
                         <span>Description two</span><br/>
                     </li>
 
                     <li>
-                        <img src="pop3.jpeg"/><span class="post-title">Title three</span><br/>
+                        <img src="/storage/blog_images/pop3.jpeg"/><span class="post-title">Title three</span><br/>
                         <span>Description three</span><br/>
                     </li>
 
                     <li id="last-post">
-                        <img src="pop4.jpeg"/><span class="post-title">Title four</span><br/>
+                        <img src="/storage/blog_images/pop4.jpeg"/><span class="post-title">Title four</span><br/>
                         <span>Description four</span><br/>
                     </li>
                 </ul>
--- resources/views/inc/posts.blade.php
+++ resources/views/inc/posts.blade.php
@@ -8,8 +8,16 @@
                         <p>{!! $post->body !!}</p>
                         <a class="more" href="{{route('posts.show', $post->id)}}"><button>Mehr<span>&raquo</span></button></a>
                     <div class="comments">Kommentare <span class="black-box">{{count($post->comments)}}</span></div>
-                    <div>
+                    </div>
                 </article>
             @endforeach
     {{$posts->links()}}
+@else 
+    <article class="post">
+        <img src="/storage/post_images/default.jpg" style="width:100%">
+            <div>
+                <p>Erstelle jetzt einen Beitrag</p>
+                <a class="more" href="{{route('posts.create')}}"><button>BEITRAG ERSTELLEN</button></a>
+            </div>
+    </article>
 @endif
