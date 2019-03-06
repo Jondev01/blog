30f1a73ca19600638fc298b4eb74aa5db2b487e9
Jonathan Wernersson <jonathan2008@live.se>
2019-03-06 11:43:15 +0100

    added welcome text

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,1 +1,14 @@
 @extends('layouts/app')
+
+@section('content')
+<div class="container">
+    <div class="card">
+        <div class="card-body">
+            <div class="card-text">
+               <h3>Create your own blog today!</h3> 
+               <a href={{route('register')}}>Join now!</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
